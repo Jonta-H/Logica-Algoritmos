7b3494cbc38b014000afe953125ddd21afc8f3d1
JONATA HENRIQUE DA SILVA CARÁ <26382@unifacef.edu.br>
2025-10-24 22:06:09 -0300

    python 24/10

---
+++ Python/dicionario.py
@@ -0,0 +1,16 @@
+dicionario = {"nome": "joao", "idade": 20}
+
+print(dicionario)
+
+x = dicionario.pop("nome")
+print(x)
+
+print(dicionario['idade'])
+
+dicionario['idade'] = 10
+
+print(dicionario['idade'])
+
+dicionario['altura'] = 1.7
+
+print(dicionario)
---
+++ Python/tupla1.py
@@ -0,0 +1,9 @@
+aluno = ("Joao", 1.7, 26)
+
+print(aluno)
+
+# Acesso posicional
+print(aluno[0])
+print(aluno[1])
+print(aluno[2])
+
